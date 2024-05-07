560e114135ff93cba911628997049cd29ca9d964
Sunbeam <mdsunbeam3.14@gmail.com>
2024-05-07 11:10:15 -0500

    uncomment examples

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
     system_message = "You are a helpful assistant."
     text = "Describe what you see in this image."
 
-    # gpt4turbo = GPT4Turbo(system_message=system_message)
-    # gpt4turbo.add_user_message(frame=logo, user_msg=text)
-    # print("GPT4Turbo: ", gpt4turbo.generate_response())
+    gpt4turbo = GPT4Turbo(system_message=system_message)
+    gpt4turbo.add_user_message(frame=logo, user_msg=text)
+    print("GPT4Turbo: ", gpt4turbo.generate_response())
 
     opus = Claude3(system_message=system_message)
     opus.add_user_message(frame=logo, user_msg=text)
     print("Claude 3 Opus: ", opus.generate_response())
 
-    # gemini_1_5_pro = Gemini(system_message=system_message)
-    # gemini_1_5_pro.add_user_message(frame=logo, user_msg=text)
-    # print("Gemini 1.5 Pro: ", gemini_1_5_pro.generate_response())
+    gemini_1_5_pro = Gemini(system_message=system_message)
+    gemini_1_5_pro.add_user_message(frame=logo, user_msg=text)
+    print("Gemini 1.5 Pro: ", gemini_1_5_pro.generate_response())
