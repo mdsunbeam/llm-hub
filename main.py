a011f14aaa7ee2fe9c1db2c924658309d75e879a
Sunbeam <mdsunbeam3.14@gmail.com>
2024-05-17 02:46:36 -0500

    added gemini flash pointer

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     MODELS = {
     "OpenAI": ["gpt-4-turbo", "gpt-4o", "gpt-3.5-turbo"], 
     "Anthropic": ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"], 
-    "Google": ["gemini-1.5-pro-latest", "gemini-pro", "gemini-pro-vision"], 
+    "Google": ["gemini-1.5-pro-latest", "gemini-pro", "gemini-pro-vision", "gemini-1.5-flash-latest"], 
     "Meta": ["llama3-70b-8192", "llama3-8b-8192"]
     }
 
